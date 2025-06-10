b62060eb184fd48695f0cce537e0c9c413d57f57
serfoll <sergiofollyvi@gmail.com>
2025-06-11 01:03:29 +0200

    list_helper.test refactor, create autor with most blog posts

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,5 @@
 const logger = require('./logger')
+const _ = require('lodash')
 
 const dummy = blogs => {
   logger.info('dummy', blogs)
@@ -8,19 +9,35 @@ const dummy = blogs => {
 const totalLikes = blogs => {
   const init = 0
   const total = blogs.length > 0 ? blogs.reduce((acc, blog) => acc + blog.likes, init) : 0
-  logger.info('sum of likes:', total)
+  // logger.info('sum of likes:', total)
   return total
 }
 
 const favoriteBlog = blogs => {
   if (blogs.length === 0) return {}
   const favorite = blogs.reduce((max, blog) => (max.likes < blog.likes ? blog : max))
-  logger.info('favorite blog:', favorite)
+  // logger.info('favorite blog:', favorite)
   return favorite
+}
+
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return {}
+
+  const simpleSort = _.groupBy(blogs, 'author')
+  const authWithMostBlogs = _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({ author: key, blogs: value.length }))
+    .maxBy('blogs')
+    .value()
+
+  logger.info('simple sort', simpleSort)
+  logger.info('grouped blogs', authWithMostBlogs)
+  return authWithMostBlogs
 }
 
 module.exports = {
   dummy,
   favoriteBlog,
+  mostBlogs,
   totalLikes,
 }
